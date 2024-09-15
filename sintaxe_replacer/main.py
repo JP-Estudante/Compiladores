cef6d2a0098129141bbbbc48e9feb8d5be15468a
João Pedro Scheffler <joaopedroscheffler@gmail.com>
2024-09-14 23:08:09 -0300

    commit

--- sintaxe_replacer/main.py
+++ sintaxe_replacer/main.py
@@ -1,3 +1,5 @@
+import os
+
 def lerEntrada(path):
     read_lines = []
     
@@ -17,7 +19,7 @@ def remove_includes(code_lines):
             
     return processed_lines
 
-def remover_semicolons(code_lines):
+def semicolons_remove(code_lines):
     processed_lines = []
     
     for line in code_lines:
@@ -27,24 +29,43 @@ def remover_semicolons(code_lines):
         
     return processed_lines
 
-def remover_brackets(code_lines):
+def brackets_remove(code_lines):
     pilha = []
     processed_lines = []
     
     for line in code_lines:
-        if '{' in line: 
-            push(pilha, '{') # Adiciona na pilha, pois encontrou `{`
-            processed_line = line.rstrip('{') # Remove do codigo o `{`
-            processed_lines.append(processed_line) # Adiciona na lista a linha, já formatada
-        elif '}' in line:
-            pop(pilha) # Remove da pilha, pois encontrou `}`
-            processed_line = line.rstrip('}') # Remove do codigo o `}`
-            processed_lines.append(processed_line) # Adiciona na lista a linha, já formatada
-        else:
-            processed_lines.append(line) # Adiciona na lista a linha, já formatada   
-             
-    return processed_lines
 
+        while '{' in line:
+            push(pilha, '{')
+            line = line.replace('{', '', 1)  # Remove o primeiro '{' encontrado
+
+        while '}' in line:
+            pop(pilha)
+            line = line.replace('}', '', 1)  # Remove o primeiro '}' encontrado
+        
+        processed_lines.append(line)  # Adiciona a linha processada
+    
+    if (is_empty(pilha)): # Verifica se a pilha esta vazia
+        return processed_lines
+    else:
+        raise IndexError("pilha não vazia, chave aberta que não foi fechada")
+
+def create_output(code_lines, path):
+    for line in code_lines:
+        file = open(path, 'a')
+        file.write('\n')
+        file.write(line)
+    file.close()
+
+def is_file_empty(path):
+    try:
+        with open(path, 'r') as file:
+            # Tenta ler o primeiro caractere
+            return file.read(1) == ''
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo '{path}' não encontrado.")
+
+  
 # FUNÇÕES DA PILHA
 def push(pilha, item):
     pilha.append(item)
@@ -58,26 +79,29 @@ def pop(pilha):
 def size(pilha):
     return len(pilha)
 
+def is_empty(pilha):
+    return len(pilha) == 0
+
 def main():
     input_path = "sintaxe_replacer/execution/input.txt"
     output_path = "sintaxe_replacer/execution/output.txt"
     pure_lines = lerEntrada(input_path) # Recebe o codigo não formatado para o python
     no_includes = remove_includes(pure_lines) # remove os `#includes` 
-    no_semicolon_lines = remover_semicolons(no_includes) # remove do codigo os `;`
-    formatted_lines = remover_brackets(no_semicolon_lines) # remove as `{}`
-    
+    no_semicolon_lines = semicolons_remove(no_includes) # remove do codigo os `;`
+    formatted_lines = brackets_remove(no_semicolon_lines) # remove as `{}`
     
     print("\nENTRADA:")
     for pure_line in pure_lines:
         print(pure_line)
     
     # Saida
-    for formatted_line in formatted_lines:
-        file = open(output_path, 'a')
-        file.write('\n')
-        file.write(formatted_line)
-    file.close()
-        
+    if is_file_empty(output_path):
+        create_output(formatted_lines, output_path)
+    else:
+        file = open(output_path, 'w')
+        file.write('')
+        create_output(formatted_lines, output_path)
+             
 if __name__ == "__main__":
     main()
     
