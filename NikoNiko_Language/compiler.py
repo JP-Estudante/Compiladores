64fb31f4dc9b013e675fca54e0bcbe07201351c1
JP-Out <joaopedroscheffler@gmail.com>
2024-10-11 18:08:53 -0300

    Commit

--- NikoNiko_Language/compiler.py
+++ NikoNiko_Language/compiler.py
@@ -1,41 +1,47 @@
+from pyparsing import line
+
+
 file_path = "io/input.niko"
 
-keywords = [
-    "mein",         # Marca o início do programa `main`
-    "modoru",       # Retorna um valor `return`
-    "hyouji",       # Imprime o texto `printf`
-    "nyuuryoku",    # Lê o texto de entrada `scanf`
-    "moshi",        # Início de uma estrutura condicional `if`
-    "sore igai",    # Alternativa na estrutura condicional `else`
-    "kurikaeshi",   # Início de uma estrutura de repetição `for`
-    "kokoromiru",   # Início de uma estrutura de repetição `while`
-    
-    # Regras de formação: Letras (a-z, A-Z) e números (0-9), não começando com número
-    "seisuu",       # Representa números inteiros `int`
-    "shousuu",      # Representa números decimais `float`
-    "mojiretsu",    # Representa sequências de caracteres `char`
-    "shingi",       # Representa valores verdadeiro/falso `bolean`
-    "mu",           # Representa uma função que não retorna valor `void`
-    "nuru"          # Representa um valor nulo `nuru`
-    "shin"          # Representa o valor verdadeiro `true`
-    "gi"            # Representa o valor falso `false`
-    
-    # Operadores Lógicos 
-    "katsu" # Representa o `&&`
-    "mata wa" # Representa o `||`
-    "dewa nai" # Representa a `!`
-]
+# Dicionário de palavras-chave e seus equivalentes em Python
+reserved_keywords = {
+    "mein": "main",             # Marca o início do programa `main`
+    "modoru": "return",         # Retorna um valor `return`
+    "hyouji": "print",          # Imprime o texto `print`
+    "nyuuryoku": "input",       # Lê o texto de entrada `input`
+    "moshi": "if",              # Início de uma estrutura condicional `if`
+    "sore igai": "else",        # Alternativa na estrutura condicional `else`
+    "kurikaeshi": "for",        # Início de uma estrutura de repetição `for`
+    "kokoromiru": "while",      # Início de uma estrutura de repetição `while`
 
-def is_keyword(str):
-    if str in keywords:
+    # Tipos de dados
+    "seisuu": "int",            # Representa números inteiros `int`
+    "shousuu": "float",         # Representa números decimais `float`
+    "mojiretsu": "str",         # Representa sequências de caracteres `str`
+    "shingi": "bool",           # Representa valores verdadeiro/falso `bool`
+    # "mu": "None",               # Representa uma função que não retorna valor `None`
+    # "nuru": "None",             # Representa um valor nulo `None`
+    "shin": "True",             # Representa o valor verdadeiro `True`
+    "gi": "False",              # Representa o valor falso `False`
+
+    # Operadores Lógicos
+    "katsu": "and",             # Representa o operador lógico `and`
+    "mata wa": "or",            # Representa o operador lógico `or`
+    "dewa nai": "not"           # Representa o operador lógico `not`
+}
+
+lines = []
+
+def is_reserved_keyword(str):
+    if str in reserved_keywords:
        return True 
     return False
 
-# Lendo o arquivo linha por linha
+
 with open(file_path, 'r', encoding='utf-8') as file:
     for line in file:
-        print(line.strip())  # strip() remove espaços em branco extras e quebras de linha
-        if 
-            print(word)
-             
-print(is_keyword('men'))
+        line.strip()
+        lines.append(line)
+
+for line in lines:
+        print(line) 
